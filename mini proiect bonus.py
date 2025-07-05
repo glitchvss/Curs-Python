eaa59c44e09b68d2a8127c1f6ec1595041396a33
glitchvss <cristianpaulemilian@yahoo.ro>
2025-07-05 14:59:44 +0300

    Create mini proiect bonus.py

---
+++ mini proiect bonus.py
@@ -0,0 +1,11 @@
+#Cere anul nașterii utilizatorului.
+
+#Calculează ce vârstă are în prezent.
+
+#Afișează vârsta obținută.
+
+anul_nasterii = int(input("Care este anul nasterii tale? "))
+an_curent = 2025
+varsta = an_curent - anul_nasterii
+print (f"tu ai {varsta} ani.")
+
