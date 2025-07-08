68279e53f775721c67aec0ea4ece39b431e611c3
glitchvss <cristianpaulemilian@yahoo.ro>
2025-07-08 20:14:16 +0300

    Create curs.py

---
+++ curs.py
@@ -0,0 +1,13 @@
+x = 5
+print(x) # x = 5
+x += 1 # x = 6 + 1
+y = x # y = 6
+x += 1 # x = 6+1=7
+print(x) # x = 7
+
+y = x**2 # y = 7**2
+print(y)
+y1 = x**2 # y = 6**2
+
+print("x =", x)
+print("y =", y)
